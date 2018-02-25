93150f96c3f4b4208c708b457ac212142b1341f9
Mike Iogansen <mikhail.iogansen@gmail.com>
2018-02-25 20:31:44 +0300

    add circle click

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var click = 0;
 
-function increment() {
-  click++;
-  redraw();
+function increment(e) {
+  var x = e.clientX - 250;
+  var y = e.clientY - 250;
+  var dist = Math.sqrt(y * y + x * x);
+
+  if (dist < 50) {
+    click++;
+    redraw();
+  }
+
 }
-var c = document.getElementById("Canvas");
+var c = document.getElementById('Canvas');
+c.addEventListener('click', increment);
 var clx = c.getContext('2d');
 
 function redraw() {
-  clx.clearRect(0,0,c.width,c.height);
-  clx.font="20px Verdana";
-  clx.fillText("Clicks: " + click,190,20)
+  clx.fillStyle = 'red';
+  clx.clearRect(0, 0, c.width, c.height);
+  clx.font = '20px Verdana';
+  clx.fillText('Clicks: ' + click, 190, 20);
+
+  clx.beginPath();
+  clx.arc(c.width / 2, c.height / 2, 50, 0, 2 * 3.14);
+  clx.fill();
 }
 redraw();
