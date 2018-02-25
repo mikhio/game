d5d38e9caf0987e718cd8f8ac5a45d8359a4e12e
Mike Iogansen <mikhail.iogansen@gmail.com>
2018-02-25 18:34:45 +0300

    add click

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var circle = document.getElementById('circle');
-var delta = 0
-setInterval(() => {
-  circle.style.right = delta + 'px';
-  circle.style.top = delta + 'px';
-  delta += 1;
-}, 10);
+var click = 0;
+
+function increment() {
+  click++;
+  redraw();
+}
+var c = document.getElementById("Canvas");
+var clx = c.getContext('2d');
+
+function redraw() {
+  clx.clearRect(0,0,c.width,c.height);
+  clx.font="20px Verdana";
+  clx.fillText("Clicks: " + click,190,20)
+}
+redraw();
