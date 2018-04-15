b5151a69fa977bfd2ed89c8fdce21ba842bd829c
Mike Iogansen <mikhail.iogansen@gmail.com>
2018-04-16 00:02:30 +0300

    base report

--- index.js
+++ index.js
@@ -1,73 +1,24 @@
-var pjs = new PointJS(810, 505, {backgroundColor: 'black'})
-var game = pjs.game;
-var mouse = pjs.mouseControl;
-mouse.initControl();
-var point = pjs.vector.point;
-var key = pjs.keyControl;
-key.initKeyControl();
-var speed = 5;
-var dx = speed;
-var dy = speed;
-var showSc = true;
-var work = true;
-
-
-var scorpio = game.newRectObject(   { 
-	x : 100, 
-	y : 100, 
-	w : 50, 
-	h : 50, 
-	fillColor : "orange", 
-});
-var sabzro = game.newRectObject(   { 
-	x : 300, 
-	y : 100, 
-	w : 50, 
-	h : 50, 
-	fillColor : "blue", 
-});
+var canvas = document.getElementById('c1');
+var ctx = canvas.getContext('2d');
 
 
 
 
+/*//ctx.fillRect(x, y, width, height);
+ctx.clearRect(0,0, 400, 200);
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = '10';
+ctx.rect(50, 10, 100, 100);
+ctx.stroke();
+ctx.fill();*/
+var x = 10;
+var y = 10;
 
-
-game.newLoop('myGame', function () {
-	if (work) {
-		if (key.isDown("A") || key.isDown("LEFT")) {
-			scorpio.move(point(-speed, 0));
-		}	
-		if (key.isDown("D") || key.isDown("RIGHT")) {
-			scorpio.move(point(speed, 0));
-		}
-		if (key.isDown("S") || key.isDown("DOWN")) {
-			scorpio.move(point(0, speed));
-		}
-		if (key.isDown("W") || key.isDown("UP")) {
-			scorpio.move(point(0, -speed));
-		}	
-	}
-
-	if (sabzro.getDistance(scorpio.getPosition()) > 15){ 
-		sabzro.moveToC(scorpio.getPosition(), 2);
-		sabzro.rotate(scorpio.getPosition());
-	}
-
-	if (sabzro.isStaticIntersect(scorpio.getStaticBoxW(0, -speed, 0, speed))){
-		showSc = false;
-		work = false;
-	}
-
-
-
-
- 
-
-
-	sabzro.draw();
-	if (showSc) {
-		scorpio.draw();
-	}
-}) 
-game.setLoop('myGame');
-game.start();
+function draw(){
+    ctx.clearRect(0,0, 400, 400);
+    ctx.fillStyle = "green";
+    ctx.fillRect(x,y,15,15);
+    x++;
+    window.requestAnimationFrame(draw);
+}
+draw();
